6cf28a539326cca4e4651ccddc73c806381d7d38
Lilo <lilo@Lilos-MacBook-Pro.local>
2023-09-27 19:14:57 -0700

    init files

---
+++ pixelfit-backend/src/main/java/build/PixelfitApplication.java
@@ -0,0 +1,13 @@
+package build;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixelfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixelfitApplication.class, args);
+	}
+
+}
---
+++ pixelfit-backend/src/test/java/build/PixelfitApplicationTests.java
@@ -0,0 +1,13 @@
+package build;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixelfitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
