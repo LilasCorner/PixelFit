30fcd894a3652bd25639c11fdfbb1b165c97441d
bcmgbokwere0 <bcmgbokwere@gmail.com>
2023-10-03 16:15:38 -0500

    Adding jsx files for Nav bar and Login/Register Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,27 @@
-import {Route, Routes,} from 'react-router-dom';
-import Main from './Pages/Main';
-import CoolDownStretchGenerator from './Pages/CoolDownStretchGenerator';
-import RandomWorkoutGenerator from './Pages/RandomWorkoutGenerator';
-import TargetedMuscleGroupGenerator from './Pages/TargetedMuscleGroupGenerator';
-import WarmupGenerator from './Pages/WarmupGenerator';
+import { Route, Routes } from "react-router-dom";
+import Main from "./Pages/Main";
+import CoolDownStretchGenerator from "./Pages/CoolDownStretchGenerator";
+import RandomWorkoutGenerator from "./Pages/RandomWorkoutGenerator";
+import TargetedMuscleGroupGenerator from "./Pages/TargetedMuscleGroupGenerator";
+import WarmupGenerator from "./Pages/WarmupGenerator";
+import Layout from "./Layout/Layout";
+import Login from "./Login/Login";
+import Register from "./Login/Register";
 
 function App() {
   return (
-<Routes>
-  <Route path="/" element={<Main/>}></Route>
-  <Route path="/CDSG" element={<CoolDownStretchGenerator/>}></Route>
-  <Route path="/RWG" element={<RandomWorkoutGenerator/>}></Route>
-  <Route path="/TMGG" element={<TargetedMuscleGroupGenerator/>}></Route>
-  <Route path="/WG" element={<WarmupGenerator/>}></Route>
-</Routes>
-  )
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Main />}></Route>
+        <Route path="/CDSG" element={<CoolDownStretchGenerator />}></Route>
+        <Route path="/RWG" element={<RandomWorkoutGenerator />}></Route>
+        <Route path="/TMGG" element={<TargetedMuscleGroupGenerator />}></Route>
+        <Route path="/WG" element={<WarmupGenerator />}></Route>
+        <Route path="/Login" element={<Login  />}></Route>
+        <Route path="/Register" element={<Register />}></Route>
+      </Routes>
+    </Layout>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/Pages/CoolDownStretchGenerator.jsx
+++ frontend/src/Pages/CoolDownStretchGenerator.jsx
@@ -1,6 +1,8 @@
 const CoolDownStretchGenerator = () => {
     return(
-<></>
+<div>
+  <p className="font-bold text-3xl text-purple-800 min-h-screen bg-blue-700 "> hello.</p>
+</div>
     )
 }
 
